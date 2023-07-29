cf64e8529399a12f84e35e272a263cb03615e3b5
gagtain <r.tis@list.ru>
2023-07-29 17:21:22 +0300

    add

---
+++ admin/components/AdminComp/DeliveryInfo.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <p>Номер доставки в системе: {{ delivery.uuid }}</p>    
+    <p>Трек номер доставки: {{ delivery.track_number }}</p>
+    <p>Статус доставки: {{ delivery.status }}</p>
+    <p>Форма отправки: {{ delivery.delivery_method }}</p>
+    <p>Куда: {{ delivery.where }}</p>
+
+  </div>
+</template>
+
+<style src="~/assets/styles/new.css"  scoped>
+</style>
+<script>
+export default {
+    props:['delivery']
+}
+</script>
+
+<style>
+
+</style>
